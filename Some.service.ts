e5bc1d8f07abae53c4bb62f7fc7ba0f12997360d
Pikachu <ashwani1609@gmail.com>
2017-01-27 18:56:19 +0530

    new changes for file..now

--- Some.service.ts
+++ Some.service.ts
@@ -11,10 +11,20 @@ import 'rxjs/add/operator/catch';
 export class Some { 
 
 constructor (private http: Http) {} 
- 176366jsonbasicInfofalse() : Observable<Comment[]> {  
-         return this.http.get(this.commentsUrl)   
-                         .map((res:Response) => res.json())  
-                         .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
  
-       
-  }  var retJson = ""
+ 
+ 
+ 
+ var retJson = {
+    "messages": {
+        "info.ADD_GUIDE": "The Guide has been added to the list of Favorite Guides.",
+        "error.REMOVE_GUIDE": "Are you sure you want to remove this Guide from your list of favorites?",
+        "error.input.SELECT_GUIDE": "Please select a Guide.",
+        "info.REMOVE_GUIDE": "The Guide has been removed from the list of Favorite Guides."
+    }
+}  
+ 
+ 
+ 176366jsonbasicInfofalse() : new Observable(observer => {  
+         observer.next(retJson);
+     }); 
--- genfile.js
+++ genfile.js
@@ -13,12 +13,12 @@ var extFileName = 'Some';
 
 var fileName = extFileName.concat(fileNameConstant);
     var jsonVal = '';
-    var url = "http://localhost:3333/rest/api/activities/176366.json?basicInfo=false";
+    
 var cookieString = 'SPRING_SECURITY_REMEMBER_ME_COOKIE=RDJ2V3lKekttMlVydGZIRHpnRDhPQT09Oks1Y1pZOVV2bSsvd0dKb2NQaGdDeWc9PQ; expires=' + new Date(new Date().getTime() + 86409000);
     var options = {
         host: 'localhost', 
         port : 8080,
-        path: '/rest/api/activities/176366.json?basicInfo=false',
+        path: '/rest/api/public/messages',
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
@@ -29,47 +29,51 @@ var cookieString = 'SPRING_SECURITY_REMEMBER_ME_COOKIE=RDJ2V3lKekttMlVydGZIRHpnR
          var output = '';
          res.setEncoding('utf8');
         res.on('data', function (chunk) {
-            console.log("chunk "+chunk);
+           // console.log("chunk "+chunk);
             output += chunk;
         });
 
         res.on('end', function() {
-            console.log("output "+output);
+            //console.log("output "+output);
             var obj = JSON.parse(output);
             jsonVal = obj;
-            //onResult(res.statusCode, obj);
+            
+            var url = "http://localhost:3333/rest/api/activities/176366.json?basicInfo=false";
+                    var pathArray = url.split("/");
+                    var lastPart = pathArray[pathArray.length-1];
+                    var resp = {"ok" : "1", "info" : "abc"};
+                    var methName = lastPart.replace(/[^a-zA-Z0-9_-]/g,'');
+                    console.log("methName "+methName);
+                    content = "import { Injectable }     from '@angular/core'; \n"+
+                    "import { Http, Response, Headers, RequestOptions } from '@angular/http'\n"+
+                    "import {Observable} from 'rxjs/Rx';\n"+"\n \n"+
+                    "import 'rxjs/add/operator/map'; \n"+
+                    "import 'rxjs/add/operator/catch';"+
+                    "\n  \n \n "+
+                    "@Injectable \n"+
+                    "export class "+extFileName+" { \n"+
+                    "\n"+
+                    "constructor (private http: Http) {} \n \n \n"+ 
+                    " \n \n var retJson = "+JSON.stringify(jsonVal, null, 4)+"  \n \n \n"+
+                    " "+methName+"() : new Observable(observer => {  \n "+
+                    "        observer.next(retJson);"+
+                    "\n  "+
+                    "   }); " ;
+                    console.log("fileName "+fileName+" extFileName "+extFileName+" fileNameConstant "+fileNameConstant);
+                    
+                    fileSys.writeFile(fileName, content, function(err) {
+
+                            if(err) {
+                                return console.log(err);
+                            }
+                            console.log("The file was saved!");
+                        }); 
+
+
         });
      });
 
-    var pathArray = url.split("/");
-    var lastPart = pathArray[pathArray.length-1];
-    var resp = {"ok" : "1", "info" : "abc"};
-    var methName = lastPart.replace(/[^a-zA-Z0-9_-]/g,'');
-    content = "import { Injectable }     from '@angular/core'; \n"+
-    "import { Http, Response, Headers, RequestOptions } from '@angular/http'\n"+
-    "import {Observable} from 'rxjs/Rx';\n"+"\n \n"+
-    "import 'rxjs/add/operator/map'; \n"+
-    "import 'rxjs/add/operator/catch';"+
-    "\n  \n \n "+
-    "@Injectable \n"+
-    "export class "+extFileName+" { \n"+
-    "\n"+
-    "constructor (private http: Http) {} \n"+
-    " "+methName+"() : Observable<Comment[]> {  \n "+
-    "        return this.http.get(this.commentsUrl)   \n "+
-    "                        .map((res:Response) => res.json())  \n "+
-    "                        .catch((error:any) => Observable.throw(error.json().error || 'Server error'));"+
-    "\n \n "+
-    "      \n  } " +
-    " var retJson = "+JSON.stringify(jsonVal, null, 4);
-    console.log("fileName "+fileName+" extFileName "+extFileName+" fileNameConstant "+fileNameConstant);
-fileSys.writeFile(fileName, content, function(err) {
-
-        if(err) {
-            return console.log(err);
-        }
-        console.log("The file was saved!");
-    }); 
+    
 
 }
 
